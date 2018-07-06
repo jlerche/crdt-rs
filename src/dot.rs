a458c7fd0f1c2422d4d84bf32d4fa0f02a6ab0cf
Jacob Lerche <jacob.lerche@gmail.com>
2018-07-05 20:13:43 -0700

    almost finishes dotset

--- src/dot.rs
+++ src/dot.rs
@@ -1,4 +1,10 @@
-pub struct Dot<T> {
+use std::cmp::Eq;
+use std::hash::Hash;
+#[derive(Hash, Eq, PartialEq, Clone)]
+pub struct Dot<T>
+where
+    T: Eq + Hash + Clone,
+{
     actor: T,
     sequence: usize,
 }
--- src/dotsset.rs
+++ src/dotsset.rs
@@ -1,14 +1,61 @@
 extern crate indexmap;
 
+use std::cmp::Eq;
+use std::hash::Hash;
+
 use self::indexmap::IndexSet;
 use dot::Dot;
 
-pub struct DotSet<T> {
-    dots: IndexSet<T>
+pub trait DotStore {
+    fn new() -> Self;
+
+    fn is_empty(&self) -> bool;
 }
 
-impl DotSet<T> {
-    fn new() -> Self {
-        DotSet {dots: IndexSet::new() }
+pub struct DotSet<T: Hash + Eq + Clone> {
+    dots: IndexSet<Dot<T>>
+}
+
+impl<T: Hash + Eq + Clone> DotStore for DotSet<T> {
+    fn new() -> DotSet<T> {
+        DotSet { dots: IndexSet::new() }
     }
+
+    fn is_empty(&self) -> bool {
+        self.dots.is_empty()
+    }
+}
+
+
+impl<T: Hash + Eq + Clone> DotSet<T> {
+    pub fn from_dots<I: IntoIterator<Item=Dot<T>>>(dots: I) -> DotSet<T> {
+        let mut dotset = Self::new();
+        for dot in dots.into_iter() {
+            dotset.add_dot(dot);
+        }
+        dotset
+    }
+
+    pub fn add_dot(&mut self, dot: Dot<T>) {
+        self.dots.insert(dot);
+    }
+
+
+    pub fn has_element(&self, dot: &Dot<T>) -> bool {
+        self.dots.contains(dot)
+    }
+
+    pub fn union(&mut self, other: DotSet<T>) {
+        self.dots = self.dots.union(&other.dots).cloned().collect();
+    }
+
+    pub fn intersection(&mut self, other: DotSet<T>) {
+        self.dots = self.dots.intersection(&other.dots).cloned().collect();
+    }
+
+    pub fn subtract(&mut self, other: DotSet<T>) {
+        self.dots = self.dots.difference(&other.dots).cloned().collect();
+    }
+    // fn subtract_causal_context <- after implementing causal_context
+    // fn fold <- maybe
 }
