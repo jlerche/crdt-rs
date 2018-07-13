5826173b4685daafc6a2342ab9a6298ad9eea006
Jacob Lerche <jacob.lerche@hpe.com>
2018-07-13 13:20:49 -0700

    Adds StateCRDT trait

--- src/crdt.rs
+++ src/crdt.rs
@@ -11,3 +11,25 @@ pub trait CRDT<T: OpMutation, U> {
 }
 
 pub trait OpMutation {}
+
+pub trait StateCRDT<T: OpMutation> {
+    fn delta_mutate(&self) -> Self;
+
+    fn merge(&mut self, other: Self) -> Self;
+
+    fn is_bottom(&self) -> bool;
+
+    fn is_inflation(&self, other: Self) -> bool;
+
+    fn is_strict_inflation(&self, other: Self) -> bool;
+
+    fn irreducible_is_strict_inflation();
+
+    fn digest(&self);
+
+    fn join_decomposition();
+
+    fn delta();
+
+
+}
